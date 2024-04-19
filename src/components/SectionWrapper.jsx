e479e7a1f333b51efd3a99913b71bc41afaa597f
paulalextech <psteane123@hotmail.com>
2024-04-19 11:59:03 +0100

    section wrapper to header/title section wrapper content

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-export default function SectionWrapper() {
-	return <div>SectionWrapper</div>;
+export default function SectionWrapper(props) {
+	const { children, header, title } = props;
+	return (
+		<section className="min-h-screen flex flex-col gap-10">
+			<div className="py-10 flex flex-col gap-2 justify-center items-center">
+				<p className="uppercase font-medium">{header}</p>
+				<h2 className="font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+					{title[0]}{' '}
+					<span className="uppercase text-orange-500">{title[1]}</span>{' '}
+					{title[2]}
+				</h2>
+			</div>
+			<h1 className="text-5xl">Strong time baby</h1>
+		</section>
+	);
 }
