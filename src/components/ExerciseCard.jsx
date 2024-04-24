7934b4245c8057310865840644f75cebbc38f7a7
paulalextech <psteane123@hotmail.com>
2024-04-24 09:53:13 +0100

    render exercise card created exercise card

---
+++ src/components/ExerciseCard.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function ExerciseCard(props) {
+	const { exercise } = props;
+	return <div>ExerciseCard</div>;
+}
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import SectionWrapper from './SectionWrapper';
 
 export default function Workout(props) {
 	const { workout } = props;
-	return <div>Workout</div>;
+	return (
+		<SectionWrapper header={'Welcome to'} title={['The', 'DANGER', 'zone']}>
+			<div className="flex flex-col gap-4">
+				{workout.map((exercise, i) =>? {
+					return(
+						<ExerciseCard/>
+					)
+				})}
+			</div>
+		</SectionWrapper>
+	);
 }
