d19b4ae9902a658eeca0fe1845eb265f45ae9397
paulalextech <psteane123@hotmail.com>
2024-04-25 09:57:44 +0100

    expanded design of workout formulation

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,3 +1,4 @@
+// import { info } from 'console';
 import React, { useState } from 'react';
 
 export default function ExerciseCard(props) {
@@ -8,10 +9,28 @@ export default function ExerciseCard(props) {
 				<h4 className="text-3xl hidden sm:inline sm:text-4xl md:text-5xl font-semi-bold">
 					{i + 1}
 				</h4>
-				<h2 className="capitalize whitespace-nowrap truncate max-w-full text-lg sm:text-xl md:text-2xl flex-1 md:text-center">
+				<h2 className="capitalize whitespace-nowrap truncate max-w-full text-lg sm:text-xl md:text-2xl flex-1 sm:text-center">
 					{exercise.name.replaceAll('_', ' ')}
 				</h2>
 				<p className="text-sm capitalize">{exercise.type}</p>
+			</div>
+			<div className="flex flex-col">
+				<h3 className="text-sm capitalize">Muscle Groups</h3>
+				<p className="capitalize">{exercise.muscles.join(' & ')}</p>
+			</div>
+			<div className="grid grid-cols-2 sm:grid-cols-4 sm:place-items-center gap-2">
+				{['reps', 'rest', 'tempo'].map((info) => {
+					return (
+						<div
+							key={info}
+							className="flex flex-col p-2 rounded border-[1.5px] border-solid w-full"
+						>
+							<h3>{info === 'reps' ? `${exercise.unit}` : info}</h3>
+
+							<p className="font-medium">{exercise[info]}</p>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
